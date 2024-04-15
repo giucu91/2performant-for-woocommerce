b25a37cf2b95a766c76207d8ba11e3fcdc02b0fd
RwkY <rwkyyy@users.noreply.github.com>
2024-04-15 18:58:56 +0300

    Initial commit

---
+++ twoo-feed.php
@@ -0,0 +1,97 @@
+<?php
+function tp_register_csv_download_endpoint() {
+	add_rewrite_rule( '^twoo-feed/?', 'index.php?products_csv_download=1', 'top' );
+	flush_rewrite_rules();
+}
+
+function tp_query_vars( $vars ) {
+	$vars[] = 'products_csv_download';
+
+	return $vars;
+}
+
+add_action( 'init', 'tp_register_csv_download_endpoint' );
+add_filter( 'query_vars', 'tp_query_vars' );
+
+function tp_trigger_csv_download() {
+	if ( get_query_var( 'products_csv_download' ) ) {
+		tp_generate_products_csv();
+		exit;
+	}
+}
+
+add_action( 'template_redirect', 'tp_trigger_csv_download' );
+
+function tp_generate_products_csv() {
+	$args     = array(
+		'status' => 'publish',
+		'limit'  => - 1,
+		'return' => 'objects'
+	);
+	$products = wc_get_products( $args );
+
+	header( 'Content-Type: text/csv; charset=utf-8' );
+	header( 'Content-Disposition: attachment; filename="products.csv"' );
+
+	$output = fopen( 'php://output', 'w' );
+
+	fputcsv( $output, array(
+		'title',
+		'description',
+		'short message',
+		'price',
+		'category',
+		'subcategory',
+		'url',
+		'image urls',
+		'product id',
+		'generate text link',
+		'brand',
+		'active',
+		'other data'
+	) );
+
+	foreach ( $products as $product ) {
+		$category_names = array();
+		$categories     = get_the_terms( $product->get_id(), 'product_cat' );
+		if ( ! empty( $categories ) ) {
+			foreach ( $categories as $category ) {
+				$category_names[] = $category->name;
+			}
+		}
+		$category    = ! empty( $category_names ) ? $category_names[0] : '';
+		$subcategory = count( $category_names ) > 1 ? $category_names[1] : '';
+
+		$images     = $product->get_gallery_image_ids();
+		$image_urls = array();
+		foreach ( $images as $image_id ) {
+			$image_urls[] = wp_get_attachment_url( $image_id );
+		}
+		$image_urls_string = implode( ',', $image_urls );
+
+		$regular_price = $product->get_regular_price();
+		$sale_price    = $product->get_sale_price();
+		$price         = $sale_price && $sale_price != $regular_price ? "$regular_price/$sale_price" : $regular_price;
+
+		$data = array(
+			$product->get_name(),
+			$product->get_description(),
+			'', // short message
+			$price,
+			$category,
+			$subcategory,
+			get_permalink( $product->get_id() ),
+			$image_urls_string,
+			$product->get_id(),
+			'0', // generate text link
+			$product->get_attribute( 'brand' ),
+			$product->is_in_stock() ? '1' : '0',
+			'' // other data
+		);
+
+		// Write product data to CSV
+		fputcsv( $output, $data );
+	}
+
+	fclose( $output );
+}
---
+++ twoo-performant-woo.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Twoo Performant @ Uprise
+ *
+ * @package       twoo-performant-uprise
+ * @author        Eduard V. Doloc
+ * @license       gplv3-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:   2Performant for WooCommerce
+ * Description:   2Performant for WooCommerce 8+
+ * Version:       1.0.3
+ * Author:        Eduard V. Doloc
+ * Author URI:    https://rwky.ro
+ * Text Domain:   twoo-performant-uprise
+ * Domain Path:   /languages
+ * License:       GPLv3 or later
+ * License URI:   https://www.gnu.org/licenses/gpl-3.0.html
+ * WC requires at least: 4.2
+ * WC tested up to: 8.8
+ * Tags: 2performant woocommerce, 2performant, 2 performant
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+//feed
+include_once 'twoo-feed.php';
+function tp_add_settings_tab( $settings_tabs ) {
+	$settings_tabs['twoo_performant_uprise'] = __( '2 Performant', 'twoo-performant-uprise' );
+
+	return $settings_tabs;
+}
+
+add_filter( 'woocommerce_settings_tabs_array', 'tp_add_settings_tab', 50 );
+
+function tp_settings_tab() {
+	woocommerce_admin_fields( get_twoo_performant_uprise_settings() );
+}
+
+add_action( 'woocommerce_settings_tabs_twoo_performant_uprise', 'tp_settings_tab' );
+
+function get_twoo_performant_uprise_settings() {
+	$feed_url = home_url( '/twoo-feed/' );
+	$settings = array(
+		'section_title'   => array(
+			'name' => __( '2 Performant Settings', 'twoo-performant-uprise' ),
+			'type' => 'title',
+			'desc' => 'Here you can set all your essential settings and get your feed url! The feed url is <a href="' . $feed_url . '" target="_blank">' . $feed_url . '</a>.',
+			'id'   => 'tp_section_title'
+		),
+		'campaign_unique' => array(
+			'name' => __( 'Campaign Unique', 'twoo-performant-uprise' ),
+			'type' => 'text',
+			'desc' => 'You can find the values <a href="https://businessleague.2performant.com/advertiser/attribution/iframe_tracking#installCode" target="_blank">here</a>; It is something like campaign_unique=abc1234, please input the value after =',
+			'id'   => 'tp_campaign_unique'
+		),
+		'confirm'         => array(
+			'name' => __( 'Confirm', 'twoo-performant-uprise' ),
+			'type' => 'text',
+			'desc' => 'You can find the values <a href="https://businessleague.2performant.com/advertiser/attribution/iframe_tracking#installCode" target="_blank">here</a>; It is something like conform=abc1234, please input the value after =',
+			'id'   => 'tp_confirm'
+		),
+		'big_bear'        => array(
+			'name' => __( 'Big Bear Attribution', 'twoo-performant-uprise' ),
+			'type' => 'text',
+			'desc' => 'You can find the value <a href="https://businessleague.2performant.com/advertiser/attribution/big_bear_attribution#section_1" target="_blank">here</a>; It is usually right after attr-2p.com/THIS_IS_THE_ID/clc/1.js',
+			'id'   => 'tp_big_bear'
+		),
+		'section_end'     => array(
+			'type' => 'sectionend',
+			'id'   => 'tp_section_end'
+		)
+	);
+
+	return apply_filters( 'tp_settings', $settings );
+}
+
+function tp_save_settings() {
+	woocommerce_update_options( get_twoo_performant_uprise_settings() );
+}
+
+add_action( 'woocommerce_update_options_twoo_performant_uprise', 'tp_save_settings' );
+
+
+function tp_add_iframe_tracking( $order_id ) {
+	$order = wc_get_order( $order_id );
+	$items = $order->get_items();
+
+	// Building the description from product names and SKUs
+	$description = array();
+	foreach ( $items as $item ) {
+		$product       = $item->get_product();
+		$product_name  = $product->get_name();
+		$sku           = $product->get_sku();
+		$description[] = "{$product_name} (SKU: {$sku})";
+	}
+	$description_string = implode( ', ', $description );
+	$sale_value         = $order->get_total() - $order->get_total_tax() - $order->get_shipping_total();
+	$transaction_id     = $order->get_order_key();
+
+	// Echo the iframe with dynamic values
+	echo "<iframe height='1' width='1' scrolling='no' marginheight='0' marginwidth='0' frameborder='0' src='https://event.2performant.com/events/salecheck?campaign_unique=" . get_option( 'tp_campaign_unique' ) . "&confirm=" . get_option( 'tp_confirm' ) . "&transaction_id={$transaction_id}&description=" . urlencode( $description_string ) . "&amount={$sale_value}'></iframe>";
+}
+
+add_action( 'woocommerce_thankyou', 'tp_add_iframe_tracking' );
+
+
+function tp_enqueue_scripts() {
+	if ( ! empty( get_option( 'tp_big_bear' ) ) ) {
+		wp_enqueue_script( 'tp_big_bear', 'https://attr-2p.com/b' . get_option( 'tp_big_bear' ) . '/clc/1.js', array(), null, true );
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'tp_enqueue_scripts' );
+
+
+function tp_modify_robots_txt( $output, $public ) {
+	$marker = "@ 2 Performant @";
+	if ( strpos( $output, $marker ) === false ) {
+		$text   = "\n# " . $marker . "\n";
+		$text   .= "Disallow: /*2pau\n";
+		$text   .= "Disallow: /*2ptt\n";
+		$text   .= "Disallow: /*2ptu\n";
+		$text   .= "Disallow: /*2prp\n";
+		$text   .= "Disallow: /*2pdlst\n";
+		$output .= $text;
+	}
+
+	return $output;
+}
+
+add_filter( 'robots_txt', 'tp_modify_robots_txt', 10, 2 );
+
+function tp_activate_plugin() {
+	flush_rewrite_rules();
+}
+
+register_activation_hook( __FILE__, 'tp_activate_plugin' );
